00c225c4a4340480006895f5b4bc2ca93e1fca67
sofia2document <sofia2document@gmail.com>
2016-07-18 13:03:35 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -3,36 +3,24 @@
 import os
 import sys
 
-from recommonmark.parser import CommonMarkParser
+#from recommonmark.parser import CommonMarkParser
+#sys.path.insert(0, os.path.abspath('..'))
+#sys.path.append(os.path.abspath('_ext'))
 
-sys.path.insert(0, os.path.abspath('..'))
-
-sys.path.append(os.path.abspath('_ext'))
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
-    'sphinxcontrib.httpdomain'
-]
+extensions = []
 templates_path = ['_templates']
 
 source_suffix = ['.rst', '.md']
-source_parsers = {
-    '.md': CommonMarkParser,
-}
 
 master_doc = 'index'
 project = u'Sofia2'
-copyright = u'2017, Indra'
+copyright = u'2016, Indra'
 version = '1.0'
 release = '1.0'
 exclude_patterns = ['_build']
-default_role = 'obj'
+#default_role = 'obj'
 pygments_style = 'sphinx'
-intersphinx_mapping = {
-    'python': ('http://python.readthedocs.io/en/latest/', None),
-    'django': ('http://django.readthedocs.io/en/latest/', None),
-    'sphinx': ('http://sphinx.readthedocs.io/en/latest/', None),
-}
+
 # This doesn't exist since we aren't shipping any static files ourselves.
 html_static_path = ['_static']
 
@@ -45,11 +33,7 @@ man_pages = [
     ('index', 'Sofia2', u'Sofia2',
      [u'Indra'], 1)
 ]
+#language = 'en'
 
-language = 'en'
 
-locale_dirs = [
-    'locale/',
-]
-gettext_compact = False
 
